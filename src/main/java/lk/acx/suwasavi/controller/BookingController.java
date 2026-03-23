cc88c70913edc28251548bf490aca72ee4671b87
AchinthaSa17 <sandaruwanachintha44@gmail.com>
2026-03-23 15:59:28 +0530

    first commit

--- src/main/java/lk/acx/suwasavi/controller/BookingController.java
+++ src/main/java/lk/acx/suwasavi/controller/BookingController.java
@@ -4,19 +4,27 @@ import lk.acx.suwasavi.entity.Booking;
 import lk.acx.suwasavi.repository.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/bookings")
+@CrossOrigin(origins = "*")
 public class BookingController {
+
     @Autowired
     private BookingRepository bookingRepository;
 
-    @PostMapping
+    @PostMapping("/create")
     public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) {
+        // Set default status to PENDING as per your DB schema
+        booking.setStatus("PENDING");
         return ResponseEntity.ok(bookingRepository.save(booking));
+    }
+
+    @GetMapping("/user/{uid}")
+    public List<Booking> getMyBookings(@PathVariable String uid) {
+        return bookingRepository.findByFirebaseUid(uid);
     }
 }
